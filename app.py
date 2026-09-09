686f739140c4151d915d0b7c01afa8c69dc7e7a2
Anshul Pal <118999286+anshullpal@users.noreply.github.com>
2026-09-09 10:20:02 +0530

    Add arithmetic functions and update main execution

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-print("Hello from Jenkins!")
-print("This is my first Python CI pipeline.")
-print("Jenkins successfully executed my Python program.")
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("Hello from Jenkins!")
+    print("2 + 3 =", add(2, 3))
+    print("5 - 2 =", subtract(5, 2))
+    print("4 * 3 =", multiply(4, 3))
